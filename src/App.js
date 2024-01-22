69cdebc08cfc59144fc446019c50c70b954974a9
DiptiVats <diptivats6@gmail.com>
2024-01-22 11:33:47 +0530

    using NavLink element and setting active styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/ErrorPages";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      errorElement: <ErrorPage />,
       children: [
         { path: "/", element: <HomePage /> },
         { path: "/products", element: <Product /> },
---
+++ src/pages/ErrorPages.js
@@ -0,0 +1,3 @@
+export default function ErrorPage() {
+  return <hi>Cannt resolve the error</hi>;
+}
