7203fc20a76cc4760e64df01ef86a261f661b13f
DiptiVats <diptivats6@gmail.com>
2024-01-22 10:10:47 +0530

    using react router

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,7 @@
-import MainNavigation from "../components/MainNavigation";
-
 export default function HomePage() {
   return (
     <>
       <h1>home Page</h1>
-      <MainNavigation />
     </>
   );
 }
